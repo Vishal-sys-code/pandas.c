8baf8d7691702281b6ae5f0bc854be7de4fb3f74
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-14 09:31:27 +0530

    Create example_series_usage.c

---
+++ examples/example_series_usage.c
@@ -0,0 +1,29 @@
+#include "series.h"
+#include <stdio.h>
+
+int main() {
+    // Create Series
+    Series *int_series = create_series("Integers", DF_INT);
+    if (!int_series) {
+        fprintf(stderr, "Failed to create integer Series\n");
+        return 1;
+    }
+
+    // Add values to integer Series
+    SeriesValue value1 = {.i_val = 10};
+    SeriesValue value2 = {.i_val = 20};
+    SeriesValue value3 = {.i_val = 30};
+
+    if (add_value(int_series, value1) != 0 || add_value(int_series, value2) != 0 || add_value(int_series, value3) != 0) {
+        fprintf(stderr, "Failed to add value to integer Series\n");
+        delete_series(int_series);
+        return 1;
+    }
+
+    // Print integer Series
+    print_series(int_series);
+
+    // Clean up
+    delete_series(int_series);
+    return 0;
+}
